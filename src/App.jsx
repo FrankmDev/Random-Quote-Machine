f5ba9f0ae88580ebfb7a9d346167f4ccf9b4dc98
FrankmDev <frankmmarindev@gmail.com>
2023-11-25 00:09:24 +0100

    Quote

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,39 @@ import "./App.css";
 import { FaQuoteLeft } from "react-icons/fa";
 import { FaQuoteRight } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-
 import quotes from "./quotes.json";
+import { useState } from "react";
 
 // Functions
 
 const getRandomQuote = () => {
   return quotes[Math.floor(Math.random() * quotes.length)];
 };
+
 // APP
 function App() {
+  const [quote, setQuote] = useState(getRandomQuote);
+  const handleQuote = () => {
+    setQuote(getRandomQuote());
+  };
   return (
     <main id="container">
       <section id="quote-box">
         <div id="quote-text">
           <FaQuoteLeft className="icon" />
-          <span id="text">
-            Life isn’t about getting and having, it’s about giving and being.
-          </span>
+          <span id="text">{getRandomQuote().quote}</span>
           <FaQuoteRight className="icon" />
         </div>
         <div id="quote-author">
-          <span id="author">Kevin Kruse</span>
+          <span id="author">{getRandomQuote().author}</span>
         </div>
         <div id="buttons">
           <a href="#" id="tweet-quote">
             <FaTwitter />
           </a>
-          <button id="new-quote">Next Quote</button>
+          <button id="new-quote" onClick={handleQuote}>
+            Next Quote
+          </button>
         </div>
       </section>
     </main>
